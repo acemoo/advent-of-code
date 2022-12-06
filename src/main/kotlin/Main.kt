7dcc0a64197d6d20d1c6fea02edb6d10208bf471
Jeffrey van der Laan <jeffrey.vanderlaan@justeattakeaway.com>
2022-12-06 15:44:59 +0100

    automagically find new days

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,4 @@
-import year_2015.Year2015
-import year_2021.Year2021
-import year_2022.Year2022
-
 fun main() {
-    listOf(
-        Year2015(),
-        Year2021(),
-        Year2022(),
-    )
+    AssignmentsFinder().getYears()
         .forEach { year -> year.solve() }
 }
