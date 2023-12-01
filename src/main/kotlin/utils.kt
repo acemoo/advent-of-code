a1d1e4ce05e0200c326911efcbe173aed5e6f908
jeffrey.vanderlaan <jeffrey.vanderlaan@justeattakeaway.com>
2023-12-01 12:01:53 +0100

    2023 Day 1: put util function in solution file so people can review it more easily in a browser.

--- src/main/kotlin/utils.kt
+++ src/main/kotlin/utils.kt
@@ -14,17 +14,3 @@ fun <T> Iterable<T>.groupConsecutiveBy(predicate: (T) -> Boolean) : List<List<T>
     }
     return groups
 }
-
-fun String.spelledDigitsToDigits() =
-    windowed(5, 1, true) {
-        return@windowed if (it.startsWith("one")) "1"
-         else if (it.startsWith("two")) "2"
-         else if (it.startsWith("three")) "3"
-         else if (it.startsWith("four")) "4"
-         else if (it.startsWith("five")) "5"
-         else if (it.startsWith("six")) "6"
-         else if (it.startsWith("seven")) "7"
-         else if (it.startsWith("eight")) "8"
-         else if (it.startsWith("nine")) "9"
-        else it.first().toString()
-    }.joinToString("")
--- src/main/kotlin/year_2023/Day1.kt
+++ src/main/kotlin/year_2023/Day1.kt
@@ -1,7 +1,5 @@
 package year_2023
 
-import spelledDigitsToDigits
-
 
 class Day1: Day(1) {
     override fun solvePart1(input: List<String>) =
@@ -23,3 +21,17 @@ class Day1: Day(1) {
     private fun getLastDigit(input: String) =
         input.last { it.isDigit() }.digitToInt()
 }
+
+fun String.spelledDigitsToDigits() =
+    windowed(5, 1, true) {
+        return@windowed if (it.startsWith("one")) "1"
+        else if (it.startsWith("two")) "2"
+        else if (it.startsWith("three")) "3"
+        else if (it.startsWith("four")) "4"
+        else if (it.startsWith("five")) "5"
+        else if (it.startsWith("six")) "6"
+        else if (it.startsWith("seven")) "7"
+        else if (it.startsWith("eight")) "8"
+        else if (it.startsWith("nine")) "9"
+        else it.first().toString()
+    }.joinToString("")
