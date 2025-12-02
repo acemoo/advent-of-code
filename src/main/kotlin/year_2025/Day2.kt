1c9b01b2cb4f867b59fc9872bf641282270a0826
jeffrey.vanderlaan <jeffrey.vanderlaan@justeattakeaway.com>
2025-12-02 10:10:14 +0100

    2025 day 2 part 2

--- src/main/kotlin/year_2025/Day2.kt
+++ src/main/kotlin/year_2025/Day2.kt
@@ -2,21 +2,29 @@ package year_2025
 
 class Day2: Day(2) {
     override fun solvePart1(input: List<String>) =
-        InvalidIDFinder(input).addUpAllInvalidIds()
+        InvalidIDFinder().parseRanges(input).addUpAllDIdsWithDuplicateSequence()
 
-    override fun solvePart2(input: List<String>): Any {
-        TODO("Not yet implemented")
-    }
+    override fun solvePart2(input: List<String>) =
+        InvalidIDFinder().parseRanges(input).addUpAllIdsWithRepatingSequence()
 
-    class InvalidIDFinder(
-        input: List<String>,
-    ) {
-        val ranges = input.first().split(",")
+    class InvalidIDFinder() {
+        var ranges = emptyList<String>()
 
-        fun addUpAllInvalidIds(): Long =
+        fun parseRanges(input: List<String>): InvalidIDFinder {
+            ranges = input.first().split(",")
+            return this
+        }
+
+        fun addUpAllIdsWithRepatingSequence(): Long =
+            addUpAllInvalidIds(::isRepeatingSequence)
+
+        fun addUpAllDIdsWithDuplicateSequence(): Long =
+            addUpAllInvalidIds(::isDuplicateSequence)
+
+        fun addUpAllInvalidIds(validatorFunction: (Long) -> Boolean): Long =
             ranges.flatMap(::getIds)
                 .mapNotNull { id ->
-                    if (!isValid(id)) {
+                    if (validatorFunction(id)) {
                         id
                     } else {
                         null
@@ -29,11 +37,32 @@ class Day2: Day(2) {
             return (from..to).map { it }.toSet()
         }
 
-        fun isValid(id: Long): Boolean {
+        fun isDuplicateSequence(id: Long): Boolean {
             val idString = id.toString()
             val firstSequence = idString.take(idString.length / 2)
             val lastSequence = idString.drop(idString.length / 2)
-            return firstSequence != lastSequence
+            return firstSequence == lastSequence
+        }
+
+        fun isRepeatingSequence(id: Long): Boolean {
+            val idString = id.toString()
+            (1 .. idString.length / 2).forEach { chunkSize ->
+                if (chunkedHasAtLeastTwoEntriesAndIsDistinct(idString, chunkSize)) {
+                    return true
+                }
+            }
+            return false
+        }
+
+        fun chunkedHasAtLeastTwoEntriesAndIsDistinct(id: String, num: Int): Boolean {
+            val chunked = id.chunked(num)
+            if (chunked.size == 1) {
+                return false
+            }
+            if (chunked.distinct().size == 1) {
+                return true
+            }
+            return false
         }
     }
 }
--- src/test/kotlin/year_2025/Day2Test.kt
+++ src/test/kotlin/year_2025/Day2Test.kt
@@ -9,26 +9,53 @@ import org.junit.jupiter.api.TestFactory
 internal class Day2Test:
     AbstractDayTest(
         Day2(),
-        1227775554,
-        0
+        1227775554L,
+        4174379265L
     ) {
 
     @Test
     fun getIds() {
-        assertThat(Day2.InvalidIDFinder(listOf("11-13")).getIds("11-13"))
+        assertThat(Day2.InvalidIDFinder().getIds("11-13"))
             .containsExactly(11, 12, 13)
     }
 
     @TestFactory
-    fun isValid() =
+    fun isDuplicateSequence() =
         listOf(
-            11 to false,
-            12 to true,
-            13 to true,
-            22 to false,
+            11L to true,
+            12L to false,
+            13L to false,
+            22L to true,
         ).map { (input, expected) ->
             DynamicTest.dynamicTest("${input}, should be $expected") {
-                assertThat(Day2.InvalidIDFinder(listOf("11-13")).isValid(input))
+                assertThat(Day2.InvalidIDFinder().isDuplicateSequence(input))
+                    .isEqualTo(expected)
+            }
+        }
+
+    @TestFactory
+    fun isRepeatingSequence() =
+        listOf(
+            11L to true,
+            12L to false,
+            1212L to true,
+        ).map { (input, expected) ->
+            DynamicTest.dynamicTest("${input}, should be $expected") {
+                assertThat(Day2.InvalidIDFinder().isRepeatingSequence(input))
+                    .isEqualTo(expected)
+            }
+        }
+
+    @TestFactory
+    fun chunkedHasAtLeastTwoEntriesAndIsDistinct() =
+        listOf(
+            Triple("11", 1, true),
+            Triple("11", 2, false),
+            Triple("12", 1, false),
+            Triple("1212", 2, true),
+        ).map { (input, chunkSize, expected) ->
+            DynamicTest.dynamicTest("$input, $chunkSize, should be $expected") {
+                assertThat(Day2.InvalidIDFinder().chunkedHasAtLeastTwoEntriesAndIsDistinct(input, chunkSize))
                     .isEqualTo(expected)
             }
         }
